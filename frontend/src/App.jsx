8cc640d4c168c87e271fd74f41a9e391f7392a62
Dan Cilliers <dan.cilliers@gmail.com>
2025-04-17 16:09:23 +0300

    Added local storage for adding favorites from the movie list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,22 @@
-import './css/App.css'
-import {Routes, Route} from "react-router-dom"
-import MovieCard from './components/MovieCard'
-import Home from './pages/Home'
-import Favorites from './pages/Favorites'
-import NavBar from './components/NavBar'
+import "./css/App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import NavBar from "./components/NavBar";
+import { MovieProvider } from "./contexts/MovieContext";
 
 function App() {
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
-      <main className='main-content'>
+      <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
-    </div>
-  )
+    </MovieProvider>
+  );
 }
 
-export default App
+export default App;
